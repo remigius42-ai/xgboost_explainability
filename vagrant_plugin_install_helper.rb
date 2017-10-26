a3797452d2eaa6b1a6552d016599f7ff39ec8411
Andreas Schmidt <remigius42-github.dcds7@aleeas.com>
2017-10-26 17:47:41 +0200

    Add virtual machine creation facilities
    Add Vagrant file depending on Ansible playbooks for setting
    up the VirtualBox VM capable of running the Jupyter notebooks.

---
+++ vagrant_plugin_install_helper.rb
@@ -0,0 +1,11 @@
+def install_plugins_if_missing(required_plugins)
+  plugins_to_install = required_plugins.select { |plugin| not Vagrant.has_plugin? plugin }
+  if not plugins_to_install.empty?
+    puts "Installing plugins: #{plugins_to_install.join(' ')}"
+    if system "vagrant plugin install #{plugins_to_install.join(' ')}"
+      exec "vagrant #{ARGV.join(' ')}"
+    else
+      abort "Installation of one or more plugins has failed. Aborting."
+    end
+  end
+end
